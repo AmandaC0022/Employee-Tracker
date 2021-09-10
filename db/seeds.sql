240d986e9ae4ce20f0b784696d7631f87de5dd0c
Amanda Morgan <AmandaC0022@gmail.com>
2021-09-10 16:22:54 -0500

    added more dummy data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,20 @@
 INSERT INTO department (name)
-VALUES ("Engineering"); 
+VALUES 
+    ("Engineering"), 
+    ("Management"),
+    ('Sales'), 
+    ("Fiance"); 
 
-INSERT INTO role (title, salary)
-VALUES ("Lead Engineer", 150000); 
+INSERT INTO role (title, salary, department_id)
+VALUES 
+    ("Lead Engineer", 150000, 2), 
+    ("Wed Developer", 80000, 1), 
+    ("Accountant", 80000, 4), 
+    ("Salesperson", 60000, 3); 
 
-INSERT INTO employee (first_name, last_name)
-VALUES ("Amanda", "Morgan"); 
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES 
+    ("Amanda", "Morgan", 2), 
+    ("Chris", "Morgan", 4), 
+    ("Caitlin", "Hendrix", 2), 
+    ("Ben", 'Hendrix', 3); 
