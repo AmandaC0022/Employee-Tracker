769afb3bbf7b01461f69645a7e21c49073a70711
Amanda Morgan <AmandaC0022@gmail.com>
2021-09-13 18:20:51 -0500

    added new file

--- server.js
+++ server.js
@@ -12,14 +12,4 @@ const db = mysql.createConnection(
   console.log('Connected to the employees_db database.')
 );
 
-// Shows the Roles table from seeds file 
-//NOTE: you can filter what exactly is shown by changing * to title, salary, etc. 
-// db.query('SELECT * FROM role', function (err, results) {
-//   if (err) {
-//     console.error(err); 
-//   }
-//   var allRoles = results; 
-//   console.table('Roles', allRoles.slice(0)); 
-// });
-
 module.exports = db; 
