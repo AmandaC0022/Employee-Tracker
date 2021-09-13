c4e7e85b35d81e788200a50c81ea9ee58fea9c85
Amanda Morgan <AmandaC0022@gmail.com>
2021-09-12 20:27:34 -0500

    now displays roles from seed data in inquirer

--- index.js
+++ index.js
@@ -64,7 +64,15 @@ updateEmployee = () => {
 }; 
 
 viewAllRoles = () => {
-    //displays the roles table 
+    // Shows the Roles table from seeds file 
+    // NOTE: you can filter what exactly is shown by changing * to title, salary, etc. 
+    db.query('SELECT * FROM role', function (err, results) {
+        if (err) {
+            console.error(err); 
+        }
+        var allRoles = results; 
+        console.table('Roles', allRoles.slice(0)); 
+        }); 
     mainScreen();
 }; 
 
